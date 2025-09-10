515392d291aba4590e6c94fbae40df0516383737
Glen Hickman <gchickma@us.ibm.com>
2025-09-10 12:04:39 -0500

    add @JsonIgnoreProperties to all internal core user models

--- src/main/java/io/boomerang/client/model/Team.java
+++ src/main/java/io/boomerang/client/model/Team.java
@@ -2,14 +2,12 @@
 package io.boomerang.client.model;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({"id", "name", "shortName", "owners", "purpose", "dateCreated", "isActive",
-    "newRelicRestApiKey", "newRelicQueryKey", "newRelicAccountId", "autoApproveRequests",
-    "privateTeam", "ldapName", "tools", "lowLevelGroupId"})
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Team {
 
   @JsonProperty("id")
